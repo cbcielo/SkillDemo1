1ff433d9b0420a4a9c2e8fdf014320291c727c4a
cbcielo <ccielo@ucsd.edu>
2022-05-01 13:56:50 -0700

    create test  and failing test

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -1,4 +1,6 @@
 public class SkillDemo1 {
 
+    public static int subtract(int a, int b){
+		return a-b;
 
 }
---
+++ SkillDemo1Tester.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo1Tester {
+
+    @Test
+    public void subtraction() {
+        assertEquals(4,SkillDemo1.subtract(8, 2)); //test that fails 
+    }
+ 
+    
+}
