79bfc650f644de50835d10f2ab2287da56096d3b
cbcielo <ccielo@ucsd.edu>
2022-05-01 14:00:13 -0700

    passing test

--- SkillDemo1Tester.java
+++ SkillDemo1Tester.java
@@ -5,7 +5,7 @@ public class SkillDemo1Tester {
 
     @Test
     public void subtraction() {
-        assertEquals(4,SkillDemo1.subtract(8, 2)); //test that fails 
+        assertEquals(4,SkillDemo1.subtract(8, 4)); //test that passes
     }
  
     
